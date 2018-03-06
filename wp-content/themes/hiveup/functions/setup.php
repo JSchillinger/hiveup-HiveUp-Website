fb221cf20ecfb2361a70cb92ec06f6b0a01ca5c6
Julian <julian@hive-up.com>
2018-03-06 12:29:00 +0800

    Search page fix
    Added archive.php
    Update core wordpress files

--- wp-content/themes/hiveup/functions/setup.php
+++ wp-content/themes/hiveup/functions/setup.php
@@ -87,11 +87,12 @@ function create_type_taxonomy() {
 	);
 }
 
-function SearchFilter($query) {
+if (!is_admin()) {
+function wpb_search_filter($query) {
 if ($query->is_search) {
-$query->set('post_type', 'page');
+$query->set('post_type', 'post');
 }
 return $query;
 }
-
-add_filter('pre_get_posts','SearchFilter');
+add_filter('pre_get_posts','wpb_search_filter');
+}
